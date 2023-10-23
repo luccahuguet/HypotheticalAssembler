648380dbe06554e2293f9441ea6e5e1ad8ed7a74
LucaBarba <barbadluca@gmail.com>
2023-10-22 22:09:56 -0300

    Update SImulador

--- simulador.cpp
+++ simulador.cpp
@@ -4,28 +4,11 @@
 #include <vector>
 #include <map>
 #include <limits>
+#include <fstream>
 
 using namespace std;
 
-// <OPCode, qnt_enderecos>
-// map<int, int> instructions_table = 
-// {
-//     {1 , 2},    // ADD
-//     {2 , 2},    // SUB
-//     {3 , 2},    // MUL
-//     {4 , 2},    // DIV
-//     {5 , 2},    // JMP
-//     {6 , 2},    // JMPN
-//     {7 , 2},    // JMPP
-//     {8 , 2},    // JMPZ
-//     {9 , 3},    // COPY
-//     {10 , 2},   // LOAD
-//     {11 , 2},   // STORE
-//     {12 , 2},   // INPUT
-//     {13 , 2},   // OUTPUT
-//     {8 , 1}     // STOP
-// };
-
+// ED que guarda data section
 // <endereco, valor>
 map<int, int> dados;
 
@@ -45,11 +28,24 @@ vector<string> split_code(string s) {
     return return_vec;
 }
 
-int main(){
+int main(int argc, char *argv[]){
+
+    string filename = argv[1];
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cout << "Error: Could not open the file." << endl;
+        return 1;
+    }
+
+    string input;
+    getline(file, input);
+
+    // Close the file
+    file.close();
 
     int PC = 0, ACC;
 
-    string input = "12 22 10 22 2 23 8 18 11 21 3 22 11 22 10 21 5 4 13 22 14 0 0 1";
     vector<string> instructions_string;
     vector<int> instructions;
 
@@ -61,30 +57,27 @@ int main(){
         instructions.push_back(stoi(instructions_string[i]));
     }
 
-
-
     // Faz uma passagem para identificar labels e encontrar section data
     // Inicializa map de dados
-
     for (int i = 0; i < instructions.size(); i++)
     {
-        
-        if((instructions[i] >= 1 && instructions[i] <= 8) || (instructions[i] >= 10 && instructions[i] <= 13) )     // Instrucoes com 2 enderecos
+        if((instructions[i] >= 1 && instructions[i] <= 4) || (instructions[i] >= 10 && instructions[i] <= 13) )     // Instrucoes com 2 enderecos
         {
             // Checa se label já está nos maps dos dados
             if(!dados.count(instructions[i+1]))
             {
-                // if(instructions[i+1] == 4)
-                    // cout << "AQUI " << i << endl;
-
                 dados.insert({instructions[i+1], instructions[instructions[i+1]]});
                 i++;
             }
             else
             {
-                i+=3;
+                i++;
             }
 
+        }
+        else if(instructions[i] >= 5 && instructions[i] <= 8)
+        {
+            i++;
         }
         else if(instructions[i] == 9)   // COPY
         {
@@ -112,16 +105,6 @@ int main(){
         
 
     }
-    
-    // cout << "tamanho: " << dados.size() << endl;
-
-    // map<int, int>::iterator it_dados = dados.begin();
-    // while(it_dados != dados.end())
-    // {
-    //     cout << it_dados->first << " " << it_dados->second << endl;
-    //     it_dados++;
-    // }
-
 
     int first_data_address = instructions.size()-1;
     map<int, int>::iterator it_dados = dados.begin();
@@ -134,25 +117,13 @@ int main(){
         
         it_dados++;
     }
-    // cout << first_data_address << endl;    
 
-
-
-
-
-
-
-
-
-
-
+    // Executa arquivo objeto
     bool encerra = false;
     for (int i = 0; i < instructions.size(); i++)
     {
-        if(encerra)
-            break;
-
-        // getchar();
+        if(instructions[i] != 12)
+            cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
 
         switch(instructions[i]) {
 
@@ -161,7 +132,7 @@ int main(){
                 PC += 2;
                 i++;
                 ACC += dados[instructions[i]];
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "ADD PC <- " << PC << " ACC <- " << ACC << endl;
                 break;
             
             case 2:     // SUB
@@ -169,7 +140,7 @@ int main(){
                 PC += 2;
                 i++;
                 ACC -= dados[instructions[i]];
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "SUB PC <- " << PC << " ACC <- " << ACC << endl;
                 break;
 
             case 3:     // MUL
@@ -177,7 +148,7 @@ int main(){
                 PC += 2;
                 i++;
                 ACC *= dados[instructions[i]];
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "MUL PC <- " << PC << " ACC <- " << ACC << endl;
                 break;
 
             case 4:     // DIV
@@ -185,85 +156,92 @@ int main(){
                 PC += 2;
                 i++;
                 ACC /= dados[instructions[i]];
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "DIV PC <- " << PC << " ACC <- " << ACC << endl;
                 break;
-            
+
             case 5:     // JMP
                 
-                PC += instructions[i+1];
-                i = PC;
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                i = instructions[i+1];
+                PC = i;
+                i-=1;
+
+                cout << "JMP PC <- " << PC << " ACC <- " << ACC << endl;
                 break;
             
             case 6:     // JMPN
                 
                 if(ACC < 0)
                 {
-                    PC = instructions[i+1];
-                    i = PC;
+                    i = instructions[i+1];
+                    PC = i;
+                    i-=1;
                 }
-                else
-                {
+                else{
                     PC += 2;
                     i++;
                 }
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+
+                cout << "JMPN PC <- " << PC << " ACC <- " << ACC << endl;
                 break;
             
             case 7:     // JUMPP
                 
                 if(ACC > 0)
                 {
-                    PC = instructions[i+1];
-                    i = PC;
+                    i = instructions[i+1];
+                    PC = i;
+                    i-=1;
                 }
-                else
-                {
+                else{
                     PC += 2;
                     i++;
                 }
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+
+                cout << "JMPP PC <- " << PC << " ACC <- " << ACC << endl;
                 break;
             
             case 8:     // JMPZ
                 
                 if(ACC == 0)
                 {
-                    PC = instructions[i+1];
-                    i = PC;
+                    i = instructions[i+1];
+                    PC = i;
+                    i-=1;
                 }
-                else
-                {
+                else{
                     PC += 2;
                     i++;
                 }
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+
+                cout << "JMPZ PC <- " << PC << " ACC <- " << ACC << endl;
                 break;
-            
+
             case 9:     // COPY
 
-                dados[i+2] = dados[instructions[i+1]];
+                dados[instructions[i+2]] = dados[instructions[i+1]];
                 PC += 3;
                 i+=2;
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "COPY PC <- " << PC << " ACC <- " << ACC << endl;
 
             break;
 
             case 10:     // LOAD
 
+                // cout << "endereco em que esta fazendo load: " << instructions[i+1] << endl;
                 ACC = dados[instructions[i+1]];
                 PC += 2;
                 i++;
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "LOAD PC <- " << PC << " ACC <- " << ACC << endl;
 
             break;
 
             case 11:     // STORE
 
+                // cout << "endereco em que esta fazendo store: " << instructions[i+1] << endl;
                 dados[instructions[i+1]] = ACC;
                 PC += 2;
                 i++;
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "STORE PC <- " << PC << " ACC <- " << ACC << endl;
 
             break;
 
@@ -272,31 +250,27 @@ int main(){
                 cin >> dados[instructions[i+1]];
                 PC += 2;
                 i++;
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "INPUT PC <- " << PC << " ACC <- " << ACC << endl;
 
             break;
 
             case 13:     // OUTPUT
                 PC += 2;
                 i++;
-                cout << "PC <- " << PC << " ACC <- " << ACC << " SAIDA: " << dados[instructions[i]] << endl;
+                cout << "OUTPUT PC <- " << PC << " ACC <- " << ACC << " SAIDA: " << dados[instructions[i]] << endl;
             break;
 
             case 14:     // STOP
                 encerra = true;
                 PC += 2;
                 i++;
-                cout << "PC <- " << PC << " ACC <- " << ACC << endl;
+                cout << "STOP PC <- " << PC << " ACC <- " << ACC << endl;
             break;
 
 
         }
 
-            // cin.ignore(std::numeric_limits<streamsize>::max(),'\n');
     }
-
-
-
 
     return 0;
 }
